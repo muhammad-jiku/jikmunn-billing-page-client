35505c423f1b75f4beda219004be7940d17b0a8d
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 13:00:31 +0600

    daisy ui setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  );
 }
 
 export default App;
