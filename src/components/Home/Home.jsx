458a8f980af19a31b80192afdc81fd89c17a3bff
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 14:00:18 +0600

    React-router initialized

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-xl text-center">
+        Welcome to the jikmunn billing page!!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
