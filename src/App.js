ea7dc7710fa2efa5223f3e0bd81c17b65a851a8c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 13:09:45 +0600

    css files modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button className="btn btn-primary">Button</button>
