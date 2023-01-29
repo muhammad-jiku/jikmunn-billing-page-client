7b102e142b8ee79dab29218629894e96c7296125
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-29 13:57:08 +0600

    navbar remodified

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="navbar bg-gray-400">
       <div className="flex-1">
-        <p className="btn btn-ghost normal-case text-xl">Jikmunn Billing</p>
+        <ul className="btn btn-ghost normal-case text-xl">
+          <li>
+            <a href="/">Jikmunn Billing</a>
+          </li>
+        </ul>
       </div>
       <div className="flex-none">
-        <p> Total Paid: 0</p>
+        <ul class="menu menu-horizontal px-1">
+          <li>
+            {' '}
+            <p>Total Cost: 0</p>{' '}
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/registration">Registration</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
