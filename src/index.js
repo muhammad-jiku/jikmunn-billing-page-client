d5fb821f8db4c0745a344bbe0fda6a67c3382d36
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-30 09:10:49 +0600

    react query initialized

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import 'react-toastify/dist/ReactToastify.css';
+// Create a client
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>{' '}
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
