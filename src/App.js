d62ddf8a76e64bd67a7fa5ce669c51a25e8538ca
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-30 08:56:43 +0600

    registration page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
-import Registration from './components/Auth/Registration';
 import Navbar from './components/Shared/Navbar';
 import NotFound from './components/Shared/NotFound';
 import LogInPage from './pages/AuthPages/LogInPage';
+import RegistrationPage from './pages/AuthPages/RegistrationPage';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Routes location={location} key={location?.pathname}>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LogInPage />} />
-        <Route path="/registration" element={<Registration />} />
+        <Route path="/registration" element={<RegistrationPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ToastContainer />
---
+++ src/pages/AuthPages/RegistrationPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Registration from '../../components/Auth/Registration';
+
+const RegistrationPage = () => {
+  return (
+    <>
+      <Registration />
+    </>
+  );
+};
+
+export default RegistrationPage;
