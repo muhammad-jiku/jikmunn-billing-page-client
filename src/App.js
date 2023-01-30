f5f8a9cda16e613b438b66c110420c613b5e5d26
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-30 08:58:22 +0600

    not found page 404

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Navbar from './components/Shared/Navbar';
-import NotFound from './components/Shared/NotFound';
+import NotFoundPage from './pages/404/NotFoundPage';
 import LogInPage from './pages/AuthPages/LogInPage';
 import RegistrationPage from './pages/AuthPages/RegistrationPage';
 import HomePage from './pages/HomePage/HomePage';
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/registration" element={<RegistrationPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <ToastContainer />
     </div>
