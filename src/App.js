6cda59d2110456a0fd25d61269ed8de7b6ea5d20
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-30 08:55:10 +0600

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
-import LogIn from './components/Auth/LogIn';
 import Registration from './components/Auth/Registration';
 import Navbar from './components/Shared/Navbar';
 import NotFound from './components/Shared/NotFound';
+import LogInPage from './pages/AuthPages/LogInPage';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
       <Routes location={location} key={location?.pathname}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/login" element={<LogInPage />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
